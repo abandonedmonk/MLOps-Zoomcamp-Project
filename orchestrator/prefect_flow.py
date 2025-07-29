7ad06fbc79d43ea43a2cb1ef14265ab754385a66
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2025-07-29 22:15:52 +0530

    Orchestrating the entire model register flow

---
+++ orchestrator/prefect_flow.py
@@ -0,0 +1,17 @@
+from prefect import flow, task
+from heart_disease_prediction.data import get_data, split_data_for_train
+from heart_disease_prediction.register import register_model
+from heart_disease_prediction.train import train_model
+from heart_disease_prediction.load_model import load_model
+
+@flow
+def full_pipeline():
+    df = get_data(path="./data/raw/processed.cleveland.data")
+    X_train, X_test, y_train, y_test, preprocessor = split_data_for_train(df)
+    _, preprocessor, paths = train_model(X_train, X_test, y_train, y_test, preprocessor)
+    paths = register_model(preprocessor, paths)
+    load_model(paths)
+
+if __name__ == "__main__":
+    full_pipeline()
+
